6c59746e4b3d3fba566117ec3b850f7fba2bc0ec
Dmitriy Merkurev <dmerkurev@avito.ru>
2017-01-29 03:10:56 +0300

    more complex example

--- app/src/main/java/ru/dimorinny/floatingsample/MainActivity.java
+++ app/src/main/java/ru/dimorinny/floatingsample/MainActivity.java
@@ -2,6 +2,11 @@ package ru.dimorinny.floatingsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingTextButton callButton = (FloatingTextButton) findViewById(R.id.call_button);
+        FloatingTextButton sendButton = (FloatingTextButton) findViewById(R.id.send_button);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "Call clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 240);
+                toast.show();
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "Send clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 240);
+                toast.show();
+            }
+        });
     }
 }
