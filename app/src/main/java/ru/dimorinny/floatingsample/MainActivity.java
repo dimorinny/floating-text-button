406e15e18e7c667eebd20b910a9b50a9781ef1c8
Dmitriy Merkurev <dmerkurev@avito.ru>
2017-02-01 03:30:30 +0300

    add snackbar layout behaviour as default

--- app/src/main/java/ru/dimorinny/floatingsample/MainActivity.java
+++ app/src/main/java/ru/dimorinny/floatingsample/MainActivity.java
@@ -1,10 +1,10 @@
 package ru.dimorinny.floatingsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
@@ -15,24 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final CoordinatorLayout container = (CoordinatorLayout) findViewById(R.id.container);
         FloatingTextButton callButton = (FloatingTextButton) findViewById(R.id.call_button);
-        FloatingTextButton sendButton = (FloatingTextButton) findViewById(R.id.send_button);
 
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, "Call clicked", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 240);
-                toast.show();
-            }
-        });
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, "Send clicked", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 240);
-                toast.show();
+                Snackbar.make(container, "Call button clicked", Snackbar.LENGTH_SHORT).show();
             }
         });
     }
