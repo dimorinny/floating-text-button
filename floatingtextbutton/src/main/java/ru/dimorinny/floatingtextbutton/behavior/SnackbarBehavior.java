684846b56867dc2573b814fa5c9e245bab989a3d
Dmitriy Merkurev <dmerkurev@avito.ru>
2017-02-20 21:34:08 +0300

    fix snackbar position when snackbar is hidden

--- floatingtextbutton/src/main/java/ru/dimorinny/floatingtextbutton/behavior/SnackbarBehavior.java
+++ floatingtextbutton/src/main/java/ru/dimorinny/floatingtextbutton/behavior/SnackbarBehavior.java
@@ -3,12 +3,21 @@ package ru.dimorinny.floatingtextbutton.behavior;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Interpolator;
 
 import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class SnackbarBehavior extends CoordinatorLayout.Behavior<FloatingTextButton> {
+
+    private static final Interpolator HIDE_INTERPOLATOR = new FastOutSlowInInterpolator();
+    private static final Long HIDE_DURATION = 250L;
+
+    private ViewPropertyAnimatorCompat animation = null;
 
     public SnackbarBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,10 +41,32 @@ public class SnackbarBehavior extends CoordinatorLayout.Behavior<FloatingTextBut
         if (child.getTranslationY() > 0) {
             return true;
         }
+        if (animation != null) {
+            animation.cancel();
+            animation = null;
+        }
 
         child.setTranslationY(
                 Math.min(0f, dependency.getTranslationY() - dependency.getHeight())
         );
         return true;
+    }
+
+    @Override
+    public void onDependentViewRemoved(
+            CoordinatorLayout parent,
+            FloatingTextButton child,
+            View dependency
+    ) {
+        if (dependency instanceof Snackbar.SnackbarLayout) {
+
+            animation = ViewCompat.animate(child)
+                    .translationY(0f)
+                    .setInterpolator(HIDE_INTERPOLATOR)
+                    .setDuration(HIDE_DURATION);
+
+            animation.start();
+        }
+        super.onDependentViewRemoved(parent, child, dependency);
     }
 }
